4334958bcd5de1c2100ac67105bffdcbb5f4436f
AlirezaPourchali <arp.joker82@gmail.com>
2025-06-05 15:33:31 +0330

    add: read the first line of the http request

--- app/main.go
+++ app/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"net"
 	"os"
@@ -11,16 +12,21 @@ var _ = os.Exit
 
 func main() {
 	fmt.Println("Logs from your program will appear here!")
-	
+
 	l, err := net.Listen("tcp", "0.0.0.0:4221")
 	if err != nil {
 		fmt.Println("Failed to bind to port 4221")
 		os.Exit(1)
 	}
-	
-	_, err = l.Accept()
+
+	var conn net.Conn
+	conn, err = l.Accept()
 	if err != nil {
 		fmt.Println("Error accepting connection: ", err.Error())
 		os.Exit(1)
 	}
+
+	fmt.Fprintf(conn, "HTTP/1.1 200 OK\r\n\r\n")
+
+	bufio.NewReader(conn).ReadString('\n')
 }
